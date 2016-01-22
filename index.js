bbdd48344c7a5fc51a996f035c49a12f1c915634
bjarkithordarson <bjarki@thordarson.is>
2016-01-22 00:40:29 +0000

    Tab switching

--- index.js
+++ index.js
@@ -1,14 +1,47 @@
 var plugins = require('electron-plugins'),
 	ipc = require('electron').ipcRenderer;
 
-console.log(plugins);
+$(document).ready(function() {
+	var context = {
+		'document' : document,
+		'pluginContent' : $("#plugin-content"),
+		'pluginTitle' : "Untitled plugin",
+		'pluginIcon' : "",
+		'UI' : {
+			addTab : function(id, caption, icon, callback) {
+				//<li class="selected"><a href="#" data-section="dashboard"><i class="fa fa-dashboard fa-lg"></i><span class="caption">Stöðuskjár</span></a></li>
 
-document.addEventListener('DOMContentLoaded', function() {
-	var context = { };
-	plugins.load(context, function(err, loaded) {
-		if(err) return console.error(err);
+				var context = this;
+
+				var tab, anchor, icon, caption;
+
+				tab = $("<li>").attr("id", "tab-" + id);
+				anchor = $("<a>").attr('href', '#');
+				icon = $("<i>").addClass('fa fa-'+icon+' fa-lg');
+				caption = $("<span>").addClass("caption").html(caption);
+
+				anchor.append(icon)
+				anchor.append(caption);
+				tab.append(anchor);
+				$("#nav-tabs").append(tab);
+
+				$(tab).click(callback);
+			},
+			loadContent : function(content) {
+				$("#plugin-content").html(content);
+				console.log(content);
+			},
+			selectTab : function(id) {
+				$("#nav-tabs li").removeClass("selected");
+				$("#nav-tabs #tab-" + id).addClass("selected");
+			}
+		}
+	};
+	plugins.load(context, function(error, loaded) {
+		if(error) return console.error(error);
 		console.log('Plugins loaded successfully.');
 	})
+
 });
 
 ipc.on('update-available', function() {
